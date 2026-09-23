6fce6b258dc2c415a2ee0da485f5d7722e5f1025
Abdul-Malik-2005 <profi.child21@gmail.com>
2026-09-23 13:12:54 +0300

    Первый комит

--- bot.py
+++ bot.py
@@ -1,2 +1,17 @@
-def func():
-    return 5
+a = 'hello python python python'
+
+# print(a[4] + a[8])
+# print(a[6:12:2])
+# print(a[::-2])
+
+# print(len(a))
+# print(a.upper())
+# print(a.lower())
+#
+# print(a.strip())
+print(a.replace('python', 'ansar', 5))
+print(a)
+# print(a.find('on'))
+# print(a.count(' '))
+# print(a.title())
+# print(a.capitalize())
